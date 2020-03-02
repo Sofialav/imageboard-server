534938ceb20048f284a74599fd8d28668a8cd8c4
Sofialav <cemeteryvixen@gmail.com>
2020-03-02 16:23:36 +0100

    auth function -> middleware

--- auth/router.js
+++ auth/router.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const bcrypt = require("bcrypt");
 const { toJWT, toData } = require("./jwt");
 const User = require("../user/model");
+const auth = require("./middleware");
 
 const router = new Router();
 // creating and sending user a token
@@ -38,26 +39,11 @@ router.post("/login", async (req, res, next) => {
   }
 });
 // secret endpoint
-router.get("/secret-endpoint", (req, res) => {
-  const auth =
-    req.headers.authorization && req.headers.authorization.split(" ");
-  if (auth && auth[0] === "Bearer" && auth[1]) {
-    try {
-      const data = toData(auth[1]);
-      res.send({
-        message: "Thanks for visiting the secret endpoint.",
-        data
-      });
-    } catch (error) {
-      res.status(400).send({
-        message: `Error ${error.name}: ${error.message}`
-      });
-    }
-  } else {
-    res.status(401).send({
-      message: "Please supply some valid credentials"
-    });
-  }
+router.get("/secret-endpoint", auth, (req, res) => {
+  console.log(req.user);
+  res.send({
+    message: `Thanks for visiting the secret endpoint ${req.user.email}.`
+  });
 });
 
 module.exports = router;
