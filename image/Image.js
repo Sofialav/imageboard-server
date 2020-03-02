a9064bdbb298051cc3242ebdfb6a4415166f311f
Sofialav <cemeteryvixen@gmail.com>
2020-03-02 11:17:07 +0100

    model Image aded

---
+++ image/Image.js
@@ -0,0 +1,9 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+const Image = db.define("image", {
+  url: { type: Sequelize.STRING, allowNull: false },
+  title: { type: Sequelize.STRING, allowNull: false }
+});
+
+module.exports = Image;
